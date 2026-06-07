0814adfe5bc208bd24bb517b3fb295ca8adc2ba6
Kumar-abhinav009 <25113cn089@glbitm.ac.in>
2026-06-07 20:06:04 +0530

    Add files via upload

---
+++ Day 6/pow.c
@@ -0,0 +1,17 @@
+//Write a program to Find x^n without pow().
+#include <stdio.h>
+
+int main() {
+    int x, n, result = 1;
+    printf("Enter the base (x): ");
+    scanf("%d", &x);
+    printf("Enter the exponent (n): ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    printf("Result: %d\n", result);
+    return 0;
+}
