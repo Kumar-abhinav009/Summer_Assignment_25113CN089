dea956e95594c822acab5f8562a34accc8924f07
Kumar-abhinav009 <25113cn089@glbitm.ac.in>
2026-06-10 07:05:10 +0530

    Add files via upload

---
+++ Day 10/characterpyramid.c
@@ -0,0 +1,18 @@
+//Write a program to Print character pyramid
+#include <stdio.h>
+
+int main() {
+    int n, i, j, k;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (k = 1; k <= 2 * i - 1; k++) {
+            printf("%c", 'A' + i - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
