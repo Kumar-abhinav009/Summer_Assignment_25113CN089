82612b667ea49b06c1e3aa866e17c2bfb8e4ac1c
Abhinav Kumar <25113cn089@glbitm.ac.in>
2026-06-23 16:56:04 +0530

    Add files via upload

---
+++ Day 21/lowertouppercase.c
@@ -0,0 +1,20 @@
+//Write a program to Convert lowercase to uppercase.
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Convert lowercase to uppercase
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        str[i] = toupper(str[i]);
+    }
+
+    printf("String in uppercase: %s", str);
+
+    return 0;
+}
